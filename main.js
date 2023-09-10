1e47128353da682712475bf0b950eee66a35fbc5
Eva-Marwaha <118869251+Eva-Marwaha@users.noreply.github.com>
2023-09-09 17:23:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = CreateCanvas(300, 300);
+    canvas.center;
+}
+
+function draw() {
+}
+
+function takeSnapshot() {
+    save('myFilterImage.png')
+}
